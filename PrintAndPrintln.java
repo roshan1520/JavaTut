b8a3bdb67df7d7b16636e1b1f4ef0a3117612d6c
portfolio <roshanvareli707@gmail.com>
2024-08-23 16:07:16 +0530

    PrintInJava

---
+++ PrintAndPrintln.java
@@ -0,0 +1,8 @@
+public class PrintAndPrintln {
+    public static void main(String[] args){
+        System.out.print("This is the print statementy. It will execute in only the single line.");
+        // this is the println statement.
+        System.out.println();
+        System.out.println("Cursor will move to the the next line and also escape to the new line");
+    }
+}
