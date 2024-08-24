f720548237f3169c35951ae814e1bf2e62bbc83d
portfolio <roshanvareli707@gmail.com>
2024-08-24 14:46:08 +0530

    ASSIGNMENT ADDED

---
+++ Assignment1.java
@@ -0,0 +1,27 @@
+public class Assignment1 {
+public static void main(String[] args) {
+    
+    // Q1. Write a program to print Hello Student on the screen
+    System.out.println("Hello Student");
+
+    // Q2. Write a program to print hello in the first line and student in the second line
+    System.out.println("Hello \nStudent");
+
+    // Q3. write a program to print "MySigG" on the screen.
+    System.out.println("\"MySirG\"");
+
+    // Q4. write a program to print value of an int variable 
+    int a = -1;
+    System.out.println(a);
+
+    float b= 5f;
+    System.out.println(b);
+
+    // Q6. write a program to check whether the given no. is positive or negative
+    if(a>=0)
+        System.out.println("it is positive.");
+    if (a<0)
+        System.out.println("it is negative");
+}
+    
+}
