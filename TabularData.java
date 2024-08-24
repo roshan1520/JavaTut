ca74f5365e80ebc283e8411292fee01a89e6ab82
portfolio <roshanvareli707@gmail.com>
2024-08-24 10:14:35 +0530

    new file added

---
+++ TabularData.java
@@ -0,0 +1,14 @@
+public class TabularData {
+    public static void main(String[] args) {
+        System.out.printf("------------------------------%n");
+        System.out.printf("     JAVA TABULAR DISPAY      %n");
+        System.out.printf("------------------------------%n");
+        System.out.printf("| %-20s | %8s | %4s | %n","CATEGORY","NAME","BITS");
+        System.out.printf("------------------------------%n");
+        System.out.printf("| %-20s | %8s | %4s | %n","FLOATING DATA","DOUBLE","64");
+
+
+
+    }
+    
+}
